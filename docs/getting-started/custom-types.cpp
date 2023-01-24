15fbdcd6748d5c788cc1ae7f03c25451536d92d2
Curve <fynnbwdt@gmail.com>
2023-01-25 00:54:37 +0100

    refactor(interoperability): custom-types example

--- docs/getting-started/custom-types.cpp
+++ docs/getting-started/custom-types.cpp
@@ -4,41 +4,52 @@
 
 using smartview = saucer::simple_smartview<saucer::serializers::json>;
 
-struct custom_type {
-  float some_float;
-  std::string some_string;
+struct custom_type
+{
+    float some_float;
+    std::string some_string;
 };
 
-namespace nlohmann {
-template <> struct adl_serializer<custom_type> {
-  static void to_json(json &j, const custom_type &c) {
-    j["some_float"] = c.some_float;
-    j["some_string"] = c.some_string;
-  }
+namespace nlohmann
+{
+    template <> struct adl_serializer<custom_type>
+    {
+        static void to_json(json &j, const custom_type &c)
+        {
+            j["some_float"] = c.some_float;
+            j["some_string"] = c.some_string;
+        }
 
-  static void from_json(const json &j, custom_type &c) {
-    c = {j["some_float"], j["some_string"]};
-  }
-};
+        static void from_json(const json &j, custom_type &c)
+        {
+            c = {j["some_float"], j["some_string"]};
+        }
+    };
 } // namespace nlohmann
 
-int main() {
-  smartview webview;
-  webview.set_size(500, 600);
-  webview.set_title("Hello World!");
+int main()
+{
+    smartview webview;
+    webview.set_size(500, 600);
+    webview.set_title("Hello World!");
 
-  webview.expose(
-      "add_random",
-      [&](custom_type c) {
-        auto random = webview.eval<float>("Math.random()").get();
-        c.some_float += random;
-        return c;
-      },
-      true);
+    webview.expose(
+        "add_random",
+        // highlight-start
+        [&](custom_type c) {
+        // highlight-end
+            auto random = webview.eval<float>("Math.random()").get();
+            // highlight-start
+            webview.eval<void>("console.log({})", c).get(); // We can now also pass the custom_type to eval!
+            // highlight-end
+            c.some_float += random;
+            return c;
+        },
+        true);
 
-  webview.serve("index.html");
-  webview.show();
-  webview.run();
+    webview.serve("index.html");
+    webview.show();
+    webview.run();
 
-  return 0;
+    return 0;
 }
